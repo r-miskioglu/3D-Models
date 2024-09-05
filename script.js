632a76f697df598fc5985eae5ad79e4ba179882c
Kalitrii <84185450+Kalitrii@users.noreply.github.com>
2024-09-05 18:30:47 +0200

    import three correctly

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
+
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
